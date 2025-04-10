05c4d38b2c4a2134aeea26d8abad41ca4bf258dc
SirMengo <msimoes0103@gmail.com>
2025-04-10 13:04:59 +0100

    Completed strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 12:25:20 by msimoes           #+#    #+#             */
-/*   Updated: 2025/04/10 12:26:39 by msimoes          ###   ########.fr       */
+/*   Updated: 2025/04/10 13:02:03 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 char	*ft_strdup(char *src)
 {
+	char *str;
+	int i;
+	int j;
 	
+	j = ft_strlen(src);
+	str = malloc(sizeof(char)* j);
+	i = 0;
+	while (src[i])
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return(str);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 10:51:47 by msimoes           #+#    #+#             */
-/*   Updated: 2025/04/10 12:34:34 by msimoes          ###   ########.fr       */
+/*   Updated: 2025/04/10 13:02:24 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 char	*ft_strchr(const char *str, int c);
+char	*ft_strdup(char *src);
 size_t	ft_strlen(char *str);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
