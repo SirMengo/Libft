bf4ad5b5d945d0cedeb375589ee0a967f5a8429a
SirMengo <msimoes0103@gmail.com>
2025-04-14 11:19:25 +0100

    Fixed norm errors

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,22 +6,22 @@
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 13:06:34 by msimoes           #+#    #+#             */
-/*   Updated: 2025/04/11 15:28:54 by msimoes          ###   ########.fr       */
+/*   Updated: 2025/04/14 11:18:12 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-	
-	unsigned char *temp_src;
-	unsigned char *temp_dest;
+	size_t			i;
+	unsigned char	*temp_src;
+	unsigned char	*temp_dest;
+
 	temp_src = (unsigned char *)src;
 	temp_dest = (unsigned char *)dest;
 	i = 0;
-	while(i < n)
+	while (i < n)
 	{
 		temp_dest[i] = temp_src[i];
 	}
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 16:06:54 by msimoes           #+#    #+#             */
-/*   Updated: 2025/04/14 11:01:27 by msimoes          ###   ########.fr       */
+/*   Updated: 2025/04/14 11:18:46 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-	unsigned char *temp_dest;
-	unsigned char *temp_src;
-	
+	size_t			i;
+	unsigned char	*temp_dest;
+	unsigned char	*temp_src;
+
 	temp_dest = (unsigned char *)dest;
 	temp_src = (unsigned char *)src;
 	i = 0;
 	if (temp_dest > temp_src)
-		while(n-- > 0)
+		while (n-- > 0)
 			temp_dest[n] = temp_src[n];
 	else
-		while(i++ < n)
+		while (i++ < n)
 			temp_dest[i - 1] = temp_src[i - 1];
 	return (dest);
 }
